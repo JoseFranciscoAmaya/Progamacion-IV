ef47cbeaeb955d355ddc577b861419125350ba23
JoseFranciscoAmaya <frankamaya0912@gmail.com>
2020-04-05 23:54:34 -0600

    FOR DOCENTES

---
+++ public/vistas/docentes/buscar-docentes.js
@@ -0,0 +1,31 @@
+var appBuscarDocentes = new Vue({
+    el:'#frm-buscar-docentes',
+    data:{
+        misdocentes:[],
+        valor:''
+    },
+    methods:{
+        buscarDocente:function(){
+            fetch(`private/modulos/docentes/procesos.php?proceso=buscarDocente&docente=${this.valor}`).then(resp=>resp.json()).then(resp=>{
+                this.misdocentes = resp;
+            });
+        },
+        modificarDocente:function(docente){
+            appdocente.docente = docente;
+            appdocente.docente.accion = 'modificar';
+        },
+        eliminarDocente:function(idDocente){
+                var comfirmacion = confirm("¿Estas seguro de eliminar el registro?");
+                if(comfirmacion){
+                    alert("El reguistro se elimino correctamente");
+                    fetch(`private/modulos/docentes/procesos.php?proceso=eliminarDocente&docente=${idDocente}`).then(resp=>resp.json()).then(resp=>{
+                        this.buscarDocente();
+                    });
+                    }  
+            
+        }
+    },
+    created:function(){
+        this.buscarDocente();
+    }
+});
